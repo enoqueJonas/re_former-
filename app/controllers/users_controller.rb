f6b3a6046c3d3f833d2e9d1b73b99f9348dad6af
enoqueJonas <enoque.macanda@gmail.com>
2024-02-19 19:21:00 +0200

    refactor: Reafctor user creation form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    p @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(users_params)
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
